e7b2262086ea4f5d47826a139f8676c36198a78e
onost <onost@g.neec.ac.jp>
2024-02-19 14:42:44 +0900

    コンソールへの出力を禁止

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include "Helper/Helper.h"
 #include "RdEngine.h"
-#ifdef _DEBUG
-int main()
-{
-#else
 #include <Windows.h>
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-#endif
 	Helper::WriteToOutput("Hello,GameProj!\n");
 	// エンジンを実行する
 	gEngine = std::make_unique<RdEngine>();
